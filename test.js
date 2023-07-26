773b0eda04b718b047e1ad8852c69d4f3e6dbc18
甘霖 <ganlin.coder@bytedance.com>
2023-07-26 15:52:32 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+function test() {
+    alert('你点击了按钮123456');
+}
+
+function ping() {
+	var Http = new XMLHttpRequest();
+	Http.open("GET", 'https://cors-demo.glitch.me/allow-cors');
+	Http.send();
+
+	Http.onreadystatechange = (e) => {
+	  console.log(Http.responseText);
+	};
+
+}
+
+
+
